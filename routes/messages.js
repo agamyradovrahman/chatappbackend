1612ccd7fc7ca8f478132dc60bb3394e02512443
rahman <rahmanagamyradov1111@gmail.com>
2023-02-05 07:31:12 +0300

    20th commit

--- routes/messages.js
+++ routes/messages.js
@@ -3,9 +3,9 @@ const { isAuthenticatedUser } = require("../middleware/authen");
 const router = require("express").Router();
 
 
-router.route("/addmsgs/").post(isAuthenticatedUser, addMessages);
+router.route("/addmsgs/").post(addMessages);
 router.route("/getmsgs/").get(getallmessages);
 router.route("/getmsgs/").get(getallmessages);
-router.route("/getsinglemsg/").get(isAuthenticatedUser, getsinglemessage);
+router.route("/getsinglemsg/").get(getsinglemessage);
 
-module.exports = router;
+module.exports = router; 
