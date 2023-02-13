dab334a5c8f952c133407b1e15a12173f7b9ef35
rahman <rahmanagamyradov1111@gmail.com>
2023-02-13 08:28:01 +0300

    21

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth")
-const messageRoutes = require("./routes/messages")
-const conRoutes = require("./routes/conn")
-const cookieParser = require("cookie-parser") 
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const conRoutes = require("./routes/conn");
+const cookieParser = require("cookie-parser"); 
+const socket = require("socket.io");
 
 const cors = require("cors");
 
 dotenv.config();
 
-const connectDatabase = async() => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URL, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
-        console.log("connected")
-    } catch (error) {
-        console.log(`Error: ${error.message}`)
-        process.exit(1)
-    }
+const connectDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("connected");
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 connectDatabase();
 
@@ -30,9 +31,31 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/msg", messageRoutes)
-app.use("/api/cons", conRoutes)
- 
-app.listen(process.env.PORT || 5000, () => {
+app.use("/api/msg", messageRoutes);
+app.use("/api/cons", conRoutes);
+
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "https://chatapp-backend-rvtt.onrender.com",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
