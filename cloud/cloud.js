bbc4904b82426a91056978f3a8493fc82c069300
rahman <rahmanagamyradov1111@gmail.com>
2023-04-20 01:04:35 +0300

    ucuns

--- cloud/cloud.js
+++ cloud/cloud.js
@@ -1,9 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 
+require("dotenv").config()
+
+const cloudname = process.env.CLOUD_NAME
+const apisecret = process.env.CLOUD_SECRET
+const apikey = process.env.CLOUD_API_KEY
+
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: parseInt(process.env.CLOUD_API_KEY),
-  api_secret: process.env.CLOUD_SECRET,
+  cloud_name: cloudname,
+  api_key: apikey,
+  api_secret: apisecret,
   secure: true,
 });
 
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const connectDatabase = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL, {
+    const connection = await mongoose.connect(process.env.MONGO_URL.toString(), {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
