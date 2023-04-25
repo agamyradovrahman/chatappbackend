5e5dbfff78cb0972d7112fe0dd3261490b189394
rahman <rahmanagamyradov1111@gmail.com>
2023-04-25 22:48:32 +0300

    rewrwerwr

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatappfrontend-5z2y.onrender.com",
     credentials: true,
   },
 });
