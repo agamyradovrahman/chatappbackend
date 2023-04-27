28909b514a025f36a87ec946b9fd9d0d832ad6db
rahman <rahmanagamyradov1111@gmail.com>
2023-04-27 05:44:58 +0300

    asdsadsadewq

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    "https://chatappfrontend-5z2y.onrender.com"
+    "https://chatappfrontend-5z2y.onrender.com",
+    "http://localhost:3000"
 ]; 
  
 module.exports = allowedOrigins; 
