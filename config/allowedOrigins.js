9e81ff44b862cfba03396eaf1ea0c25715efb195
rahman <rahmanagamyradov1111@gmail.com>
2023-04-30 03:28:58 +0300

    dasdasdnbmnbc55

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-    "https://polite-sunshine-4d436e.netlify.app",
-    "https://chatappfrontend-5z2y.onrender.com/"
+    "https://chatappfrontend-5z2y.onrender.com"
     
 ]; 
   
