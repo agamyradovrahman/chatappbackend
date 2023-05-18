52d68be591fd8d411a129d07aa0ee205cb1646a4
rahman <rahmanagamyradov1111@gmail.com>
2023-05-18 10:43:25 +0300

    backend 3

--- cloud/cloud.js
+++ cloud/cloud.js
@@ -7,11 +7,11 @@ const apisecret = process.env.CLOUD_SECRET
 const apikey = process.env.CLOUD_API_KEY
 
 cloudinary.config({
-  cloud_name: cloudname,
+  cloud_name: "dw51bz9id",
   api_key: 346597522261813,
   api_secret: "xXhyExlc8pm_wxbU8s2dlOx7KBo",
   secure: true,
 }); 
-
+ 
 
 module.exports = cloudinary; 
